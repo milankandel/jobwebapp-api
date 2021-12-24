05efe8d4771c5ce9343ab324b6cfe80b705041a0
milankandel <milankandel9@gmail.com>
2021-12-24 17:38:58 +0545

    getJobById and Slug added

--- Routes/jobs.js
+++ Routes/jobs.js
@@ -1,12 +1,14 @@
-const { createJobs, fetchAllJobs, deleteAllJobs, deleteById, findJobByRadiusAndDistance } = require('../controller/job')
+const { createJobs, fetchAllJobs, deleteAllJobs, deleteById, findJobByRadiusAndDistance, findJobAndUpdate, getJobById } = require('../controller/job')
 
 const jobRouter=require('express').Router()
 
 jobRouter.route("/jobs").get(fetchAllJobs)
+jobRouter.route("/job/:id/:slug").get(getJobById)
 jobRouter.route("/job/new").post(createJobs)
 jobRouter.route("/jobs/delete").delete(deleteAllJobs)
 jobRouter.route("/job/delete/:id").delete(deleteById)
 jobRouter.route("/jobs/:zipcode/:distance").get(findJobByRadiusAndDistance)
+jobRouter.route("/job/update/:id").put(findJobAndUpdate)
 
 module.exports=jobRouter
 
--- controller/job.js
+++ controller/job.js
@@ -50,11 +50,57 @@ exports.findJobByRadiusAndDistance=async(req,res,next)=>{
 
 }
 
+
+exports.findJobAndUpdate=async(req,res,next)=>{
+    const id=req.params.id;
+    let job=await Job.findById(id)
+    console.log(job)
+    if(!job){
+        return res.status(404).json({
+            success:true,
+            message:'Job with that id doesnot exist'
+        })
+      
+    }
+    const updatedJob=await Job.findByIdAndUpdate(id,req.body,{
+        runValidators:true,
+        new:true,
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success:true,
+        data:updatedJob,
+        message:'Job updated'
+    })
+}
+
 exports.deleteById=async(req,res,next)=>{
     const id=req.params.id;
+    const data=await Job.findById(id)
+    if(!data){
+        return res.status(404).json({
+            message:'That job doesnot exist.You cant delete',
+            success:true
+        })
+    }
     const job=await Job.findByIdAndDelete(id)
     res.status(200).json({
         success:true,
         message: `Job with ${id} deleted`
+    })
+}
+
+
+exports.getJobById=async(req,res,next)=>{
+    const job=await Job.find({$and:[{_id:req.params.id},{slug:req.params.slug}]})
+    if(!job || job.length==0){
+        return res.status(404).json({
+            success:true,
+            message:'Job with that id and slug is not present'
+        })
+    }
+    return res.status(200).json({
+        success:true,
+        data:job
     })
 }
