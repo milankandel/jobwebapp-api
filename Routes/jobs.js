bdb118c2f48bf8458206904d21fa76d07e06bae2
milankandel <milankandel9@gmail.com>
2021-12-24 11:43:42 +0545

    new commit

--- Routes/jobs.js
+++ Routes/jobs.js
@@ -5,7 +5,7 @@ const jobRouter=require('express').Router()
 jobRouter.route("/jobs").get(fetchAllJobs)
 jobRouter.route("/job/new").post(createJobs)
 jobRouter.route("/jobs/delete").delete(deleteAllJobs)
-jobRouter.route("/job/delete/:id").delete(deleteByIds)
+jobRouter.route("/job/delete/:id").delete(deleteById)
 
 module.exports=jobRouter
 
